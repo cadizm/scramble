100fde2e89cf161f9d9b0c78b59248b39a44a780
Michael Cadiz <michael.cadiz@gmail.com>
2012-05-06 00:25:09 -0700

    Htmlify output

--- scramble.py
+++ scramble.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+#
 #  0  1  2  3
 #  4  5  6  7
 #  8  9 10 11
@@ -32,6 +33,19 @@ W = open('words.txt', 'r').read()
 # Corpus as an array of strings
 D = W.split()
 
+ARR = { -1 : '&larr;',
+        -5 : '&nwarr;',
+        -4 : '&uarr;',
+        -3 : '&nearr;',
+         1 : '&rarr;',
+         5 : '&searr;',
+         4 : '&darr;',
+         3 : '&swarr;' }
+
+FLAGS = { 'tr': 0
+        }
+
+
 
 class Vertex:
     'Representation of vertex in graph'
@@ -56,8 +70,8 @@ def dfs(u, S, L):
         S.pop()
         L.pop()
         return
-    if s in D:
-        process(s, L)
+    if s in D and len(s) >= 6:
+        process(S, L)
     u.visited = True
     for v in u.adjacent_vertices:
         if not v.visited:
@@ -67,9 +81,29 @@ def dfs(u, S, L):
     L.pop()
 
 
-def process(s, L):
+def process(S, L):
     'Processing for valid words'
-    print s, ' '.join(str(i) for i in L)
+    if FLAGS['tr'] == 0:
+        print '<tr>'
+    FLAGS['tr'] += 1
+    print '<td><ol>'
+    for k in A.keys():
+        if k in L:
+            i = L.index(k)
+            c = S[i]
+            if i + 1 < len(L):
+                if i == 0:
+                    print '<li style="background-color: #00FF00">{0} {1}</li>'.format(c, ARR[L[i + 1] - L[i]])
+                else:
+                    print '<li>{0} {1}</li>'.format(c, ARR[L[i + 1] - L[i]])
+            else:
+                print '<li>{0} {1}</li>'.format(c, '&#8226;')
+        else:
+                print '<li>{0}</li>'.format('&nbsp;')
+    print '</ol></td>'
+    if FLAGS['tr'] == 2:
+        print '</tr>'
+        FLAGS['tr'] = 0
 
 
 def prune(s):
@@ -88,9 +122,18 @@ def build_graph(S):
 
 def solve(S):
     'Find words in string S a la Scramble with Friends'
+    print """<html>
+    <head>
+        <link href="scramble.css" rel="stylesheet" />
+    </head>
+    <body>
+        <table>"""
     for v in build_graph(S):
         dfs(v, [], [])
+    print """        </table>
+</body>
+</html>"""
 
 
 if __name__ == '__main__':
-    solve('skdmnetrnisacpdy'.lower())
+    solve('desuerneptapfeuw'.lower())
