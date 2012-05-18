3e3223cffd332d14b4b006eed25305f32c81c7c9
Michael Cadiz <michael.cadiz@gmail.com>
2012-05-17 23:58:07 -0700

    General cleanup
    Moved globals `A' and `POINTS' to members of class `Constants'
    Cleanup docstrings
    Cleaned up debug output
    Removed memcached usage from __main__()

--- scramble.py
+++ scramble.py
@@ -17,31 +17,32 @@ DEBUG = True
 # 12 13 14 15
 #
 
-# Adjacency list for graph
-A = { 0 : [ 1,  4,  5],
-      1 : [ 0,  2,  4,  5,  6],
-      2 : [ 1,  3,  5,  6,  7],
-      3 : [ 2,  6,  7], 
-      4 : [ 0,  1,  5,  8,  9],
-      5 : [ 0,  1,  2,  4,  6,  8,  9, 10],
-      6 : [ 1,  2,  3,  5,  7,  9, 10, 11],
-      7 : [ 2,  3,  6, 10, 11],
-      8 : [ 4,  5,  9, 12, 13],
-      9 : [ 4,  5,  6,  8, 10, 12, 13, 14],
-     10 : [ 5,  6,  7,  9, 11, 13, 14, 15],
-     11 : [ 6,  7, 10, 14, 15],
-     12 : [ 8,  9, 13],
-     13 : [ 8,  9, 10, 12, 14],
-     14 : [ 9, 10, 11, 13, 15],
-     15 : [10, 11, 14] }
-
-
-# Letter to point mapping
-POINTS = {  'a' :  1, 'b' :  4, 'c' :  4, 'd' :  2, 'e' :  1, 'f' :  4,
-            'g' :  3, 'h' :  3, 'i' :  1, 'j' : 10, 'k' :  5, 'l' :  2,
-            'm' :  4, 'n' :  2, 'o' :  1, 'p' :  4, 'q' : 10, 'r' :  1,
-            's' :  1, 't' :  1, 'u' :  2, 'v' :  5, 'w' :  4, 'x' :  8,
-            'y' :  3, 'z' : 10, }
+class Constants():
+    # Adjacency list for graph
+    A = { 0 : [ 1,  4,  5],
+          1 : [ 0,  2,  4,  5,  6],
+          2 : [ 1,  3,  5,  6,  7],
+          3 : [ 2,  6,  7], 
+          4 : [ 0,  1,  5,  8,  9],
+          5 : [ 0,  1,  2,  4,  6,  8,  9, 10],
+          6 : [ 1,  2,  3,  5,  7,  9, 10, 11],
+          7 : [ 2,  3,  6, 10, 11],
+          8 : [ 4,  5,  9, 12, 13],
+          9 : [ 4,  5,  6,  8, 10, 12, 13, 14],
+         10 : [ 5,  6,  7,  9, 11, 13, 14, 15],
+         11 : [ 6,  7, 10, 14, 15],
+         12 : [ 8,  9, 13],
+         13 : [ 8,  9, 10, 12, 14],
+         14 : [ 9, 10, 11, 13, 15],
+         15 : [10, 11, 14] }
+    
+    
+    # Letter to point mapping
+    Points = { 'a' :  1, 'b' :  4, 'c' :  4, 'd' :  2, 'e' :  1, 'f' :  4,
+               'g' :  3, 'h' :  3, 'i' :  1, 'j' : 10, 'k' :  5, 'l' :  2,
+               'm' :  4, 'n' :  2, 'o' :  1, 'p' :  4, 'q' : 10, 'r' :  1,
+               's' :  1, 't' :  1, 'u' :  2, 'v' :  5, 'w' :  4, 'x' :  8,
+               'y' :  3, 'z' : 10, }
 
 
 class Vertex():
@@ -60,9 +61,9 @@ class Vertex():
 def dfs(u, min_len, W, D, S, L, R):
     """Depth first search rooted at u, with dead end pruning.
 
-    DFS started at graph rooted at vertex u. W and D contain valid the valid
-    word corpuses and should not be modified. S and L are used for bookkeeping
-    and should also not be modified. Solution is returned via output param R,
+    DFS started at graph rooted at vertex u. W and D contain the valid word
+    corpuses and should not be modified. S and L are used for bookkeeping and
+    should also not be modified. Solution is returned via output param R,
     containing words found with length >= min_len"""
     S.append(u.label)
     L.append(u.index)
@@ -92,10 +93,10 @@ def prune(s, W):
 
 
 def build_graph_path(S):
-    'Build graph V and critical vertices P from input string S'
-    V = [Vertex(k, S[k]) for k in sorted(A.keys())]
+    'Build graph of vertices V from input string S'
+    V = [Vertex(k, S[k]) for k in sorted(Constants.A.keys())]
     for i, v in enumerate(V):
-        v.adjacent_vertices = [V[j] for j in A[i]]
+        v.adjacent_vertices = [V[j] for j in Constants.A[i]]
     return V
 
 
@@ -106,7 +107,7 @@ def order(R):
     Higher rank is given to words of length >= 8"""
     for i, r in enumerate(R):
         word, indices = r
-        length, score = len(word), sum(map(lambda x: POINTS[x], word))
+        length, score = len(word), sum(map(lambda x: Constants.Points[x], word))
         R[i] = (length, score, word, indices)
     def rank(x, y):
         length_x, length_y = x[0], y[0]
@@ -132,36 +133,25 @@ def solve(S, _W, min_len=6, fmt=None):
         return True
     D = filter(keep_word, _W.split())
     W = '\n'.join(D)
-    if DEBUG:
-        print >> sys.stderr, 'W: {0}'.format(len(W))
-        print >> sys.stderr, 'D: {0}'.format(len(D))
     V = build_graph_path(S)
     R = []
     for v in V:
         dfs(v, min_len, W, D, [], [], R)
+    R = order(R)
+    if DEBUG:
+        print >> sys.stderr, 'S: {0}'.format(S)
+        print >> sys.stderr, 'W: {0}'.format(len(W))
+        print >> sys.stderr, 'D: {0}'.format(len(D))
+        for r in R:
+            print >> sys.stderr, r
     if fmt == 'json':
-        return json.dumps([{'word': r[2], 'indices': r[3]} for r in order(R)],
+        return json.dumps([{'word': r[2], 'indices': r[3]} for r in R],
                 separators=(',',':'))
     else:
-        return order(R)
+        return R
 
 
 if __name__ == '__main__':
-    mc = memcache.Client(['127.0.0.1:11211'], debug=0)
-    _W1 = mc.get('_W1')
-    _W2 = mc.get('_W2')
-    if not _W1 or not _W1:
-        _W1 = open('words_gte_6-1.txt', 'r').read()
-        _W2 = open('words_gte_6-2.txt', 'r').read()
-        if not mc.set('_W1', _W1) or not mc.set('_W2', _W2):
-            print 'Error: couldnt set dictionary'
-            sys.exit()
-    _W = _W1 + _W2
+    _W = open('words.txt', 'r').read()
     _S = 'lawimetkeneouasl'
-    _R = mc.get(_S)
-    if not _R:
-        _R = solve(_S.lower(), _W)
-        mc.set(_S, _R)
-    if DEBUG:
-        for r in _R:
-            print >> sys.stderr, r
+    _R = solve(_S.lower(), _W)
